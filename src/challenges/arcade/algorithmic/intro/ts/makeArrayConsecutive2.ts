44bc3bdc7fc45cf86f505188dfba11297ed96e50
Viacheslav Turovskyi <aeworxet@protonmail.com>
2020-10-30 14:51:54 +0200

    Added 'Arcade, Intro, 6. makeArrayConsecutive2'

---
+++ src/challenges/arcade/algorithmic/intro/ts/makeArrayConsecutive2.ts
@@ -0,0 +1,33 @@
+/**
+ * Solution to a challenge
+ * 'CodeSignal, Arcade, Intro, 6. makeArrayConsecutive2'.
+ *
+ * Ratiorg got statues of different sizes as a present from CodeMaster for his
+ * birthday, each statue having an non-negative integer size. Since he likes to
+ * make things perfect, he wants to arrange them from smallest to largest so
+ * that each statue will be bigger than the previous one exactly by 1. He may
+ * need some additional statues to be able to accomplish that. Help him figure
+ * out the minimum number of additional statues needed.
+ *
+ * @param {number[]} statues
+ * An array of distinct non-negative integer numbers.
+ * Guaranteed constraints:
+ * 1 ≤ statues.length ≤ 10,
+ * 0 ≤ statues[i] ≤ 20.
+ *
+ * @returns {number}
+ * Integer. The minimal number of statues that need to be added to existing
+ * statues such that it contains every integer size from an interval [L, R] (for
+ * some L, R) and no other sizes.
+ *
+ * @author Viacheslav Turovskyi
+ *
+ * {@link https://app.codesignal.com/profile/rexindan|CodeSignal}
+ * {@link https://github.com/rexindan|GitHub}
+ */
+
+function makeArrayConsecutive2(statues: number[]): number {
+  return Math.max(...statues) - Math.min(...statues) - statues.length + 1;
+}
+
+module.exports = makeArrayConsecutive2;
---
+++ test/challenges/arcade/algorithmic/intro/js/makeArrayConsecutive2.test.js
@@ -0,0 +1,38 @@
+'use strict';
+/**
+ * Jest test for
+ * Solution to a challenge
+ * 'CodeSignal, Arcade, Intro, 6. makeArrayConsecutive2'.
+ */
+
+import { describe, expect, it } from '@jest/globals';
+import * as basePath from 'app-root-path';
+const adjacentElementsProduct = require(__filename
+  .replace(basePath.resolve('test'), basePath.resolve('src'))
+  .replace('.test.js', '.js'));
+/* eslint-disable max-len */
+describe('Test 1', () => {
+  it('For adjacentElementsProduct([6, 2, 3, 8]), the output should be 3', () => {
+    expect(adjacentElementsProduct([6, 2, 3, 8])).toBe(3);
+  });
+});
+describe('Test 2', () => {
+  it('For adjacentElementsProduct([0, 3]), the output should be 2', () => {
+    expect(adjacentElementsProduct([0, 3])).toBe(2);
+  });
+});
+describe('Test 3', () => {
+  it('For adjacentElementsProduct([5, 4, 6]), the output should be 0', () => {
+    expect(adjacentElementsProduct([5, 4, 6])).toBe(0);
+  });
+});
+describe('Test 4', () => {
+  it('For adjacentElementsProduct([6, 3]), the output should be 2', () => {
+    expect(adjacentElementsProduct([6, 3])).toBe(2);
+  });
+});
+describe('Test 5', () => {
+  it('For adjacentElementsProduct([1]), the output should be 0', () => {
+    expect(adjacentElementsProduct([1])).toBe(0);
+  });
+});
