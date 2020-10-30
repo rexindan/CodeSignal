5536add50680e1ab7b6c35b7a97a5007d9d27522
Viacheslav Turovskyi <aeworxet@protonmail.com>
2020-10-30 15:43:07 +0200

    Added 'Arcade, Intro, 10. commonCharacterCount'

---
+++ src/challenges/arcade/algorithmic/intro/js/commonCharacterCount.js
@@ -0,0 +1,34 @@
+'use strict';
+/**
+ * Solution to a challenge
+ * 'CodeSignal, Arcade, Intro, 10. commonCharacterCount'.
+ *
+ * Given two strings, find the number of common characters between them.
+ *
+ * @param {string} s1
+ * A string consisting of lowercase English letters.
+ * Guaranteed constraints:
+ * 1 ≤ s1.length < 15.
+ *
+ * @param {string} s2
+ * A string consisting of lowercase English letters.
+ * Guaranteed constraints:
+ * 1 ≤ s2.length < 15.
+ *
+ * @returns {number}
+ * Integer.
+ *
+ * @author Viacheslav Turovskyi
+ *
+ * {@link https://app.codesignal.com/profile/rexindan|CodeSignal}
+ * {@link https://github.com/rexindan|GitHub}
+ */
+
+function commonCharacterCount(s1, s2) {
+  for (let i = 0; i < s1.length; i++) {
+    s2 = s2.replace(s1[i], '&');
+  }
+  return s2.replace(/[^&]/g, '').length;
+}
+
+module.exports = commonCharacterCount;
---
+++ test/challenges/arcade/algorithmic/intro/ts/commonCharacterCount.test.ts
@@ -0,0 +1,37 @@
+/**
+ * Jest test for
+ * Solution to a challenge
+ * 'CodeSignal, Arcade, Intro, 10. commonCharacterCount'.
+ */
+
+import { describe, expect, it } from '@jest/globals';
+import * as basePath from 'app-root-path';
+const commonCharacterCount = require(__filename
+  .replace(basePath.resolve('test'), basePath.resolve('src'))
+  .replace('.test.ts', '.ts'));
+/* eslint-disable max-len */
+describe('Test 1', () => {
+  it('For commonCharacterCount("aabcc", "adcaa"), the output should be 3', () => {
+    expect(commonCharacterCount('aabcc', 'adcaa')).toBe(3);
+  });
+});
+describe('Test 2', () => {
+  it('For commonCharacterCount("zzzz", "zzzzzzz"), the output should be 4', () => {
+    expect(commonCharacterCount('zzzz', 'zzzzzzz')).toBe(4);
+  });
+});
+describe('Test 3', () => {
+  it('For commonCharacterCount("abca", "xyzbac"), the output should be 3', () => {
+    expect(commonCharacterCount('abca', 'xyzbac')).toBe(3);
+  });
+});
+describe('Test 4', () => {
+  it('For commonCharacterCount("a", "b"), the output should be 0', () => {
+    expect(commonCharacterCount('a', 'b')).toBe(0);
+  });
+});
+describe('Test 5', () => {
+  it('For commonCharacterCount("a", "aaa"), the output should be 1', () => {
+    expect(commonCharacterCount('a', 'aaa')).toBe(1);
+  });
+});
