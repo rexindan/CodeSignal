3d52b0c523dddbd3dba31b186cd8efbcc39aa03e
Viacheslav Turovskyi <aeworxet@protonmail.com>
2020-10-30 14:18:55 +0200

    Added 'Arcade, The Core, 8. Phone Call'

---
+++ src/challenges/arcade/algorithmic/the-core/js/phoneCall.js
@@ -0,0 +1,64 @@
+'use strict';
+/**
+ * Solution to a challenge
+ * 'CodeSignal, Arcade, The Core, 8. Phone Call'.
+ *
+ * Some phone usage rate may be described as follows:
+ *   - first minute of a call costs min1 cents,
+ *   - each minute from the 2nd up to 10th (inclusive) costs min2_10 cents
+ *   - each minute after 10th costs min11 cents.
+ *
+ * You have s cents on your account before the call. What is the duration of the
+ * longest call (in minutes rounded down to the nearest integer) you can have?
+ *
+ * @param {number} min1
+ * Integer.
+ * Guaranteed constraints:
+ * 1 ≤ min1 ≤ 10.
+ *
+ * @param {number} min2_10
+ * Integer.
+ * Guaranteed constraints:
+ * 1 ≤ min2_10 ≤ 10.
+ *
+ * @param {number} min11
+ * Integer.
+ * Guaranteed constraints:
+ * 1 ≤ min11 ≤ 10.
+ *
+ * @param {number} s
+ * Integer.
+ * Guaranteed constraints:
+ * 2 ≤ s ≤ 500.
+ *
+ * @returns {number}
+ * Integer.
+ *
+ * @author Viacheslav Turovskyi
+ *
+ * {@link https://app.codesignal.com/profile/rexindan|CodeSignal}
+ * {@link https://github.com/rexindan|GitHub}
+ */
+
+function phoneCall(min1, min2_10, min11, s) {
+  let mins;
+  if (min1 > s) {
+    mins = 0;
+    return mins;
+  } else {
+    mins = 1;
+    s -= min1;
+  }
+  if (min2_10 * 9 <= s) {
+    mins += 9;
+    s -= min2_10 * 9;
+  } else {
+    mins += parseInt(s / min2_10, 10);
+  }
+  if (mins === 10) {
+    mins += parseInt(s / min11, 10);
+  }
+  return mins;
+}
+
+module.exports = phoneCall;
---
+++ test/challenges/arcade/algorithmic/the-core/js/phoneCall.test.js
@@ -0,0 +1,43 @@
+'use strict';
+/**
+ * Jest test for
+ * Solution to a challenge
+ * 'CodeSignal, Arcade, The Core, 8. Phone Call'.
+ */
+
+import { describe, expect, it } from '@jest/globals';
+import * as basePath from 'app-root-path';
+const phoneCall = require(__filename
+  .replace(basePath.resolve('test'), basePath.resolve('src'))
+  .replace('.test.js', '.js'));
+
+describe('Test 1', () => {
+  it('For phoneCall(3, 1, 2, 20), the output should be 14', () => {
+    expect(phoneCall(3, 1, 2, 20)).toBe(14);
+  });
+});
+describe('Test 2', () => {
+  it('For phoneCall(2, 2, 1, 2), the output should be 1', () => {
+    expect(phoneCall(2, 2, 1, 2)).toBe(1);
+  });
+});
+describe('Test 3', () => {
+  it('For phoneCall(10, 1, 2, 22), the output should be 11', () => {
+    expect(phoneCall(10, 1, 2, 22)).toBe(11);
+  });
+});
+describe('Test 4', () => {
+  it('For phoneCall(2, 2, 1, 24), the output should be 14', () => {
+    expect(phoneCall(2, 2, 1, 24)).toBe(14);
+  });
+});
+describe('Test 5', () => {
+  it('For phoneCall(1, 2, 1, 6), the output should be 3', () => {
+    expect(phoneCall(1, 2, 1, 6)).toBe(3);
+  });
+});
+describe('Test 6', () => {
+  it('For phoneCall(10, 10, 10, 8), the output should be 0', () => {
+    expect(phoneCall(10, 10, 10, 8)).toBe(0);
+  });
+});
